3b2aa9a548518d7a1733698d518514feec6b1404
Rob Downing <redowning@gmail.com>
2025-10-28 23:17:10 -0700

    Initial Xcode project setup

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rob Downing on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
